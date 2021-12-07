61ecdd1562526e0ec15fc38888383567ee324906
ElsongMaster <elsongmaster@gmail.com>
2021-12-07 20:22:44 +0100

    fin challenge reste dark mode + drag and drop

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,290 @@
 <template>
   <div id="app">
+    <div class="main-container">
+      <div class="header">
+        <div class="container-todo"><span>todo</span></div>
+        <div class="containerDay-Night">
+          <img src="./assets/icon-moon.svg" alt="" />
+        </div>
+      </div>
+      <div class="newTodo">
+        <input
+          type="radio"
+          id="huey"
+          name="drone"
+          value="huey"
+          class="input-radio"
+        />
+        <input
+          type="text"
+          placeholder="Create a new todo.."
+          v-on:keyup.enter="createDataItemTodo()"
+          class="input-text"
+        />
+      </div>
+      <div class="container-todolist">
+        <TodoItem
+          v-for="(item, index) in tabTodoItem"
+          :dataObject="item"
+          :key="index"
+          @setIndexObject="getIndexObj"
+          @checkRadio="updateData"
+        />
+      </div>
+      <div class="container-options">
+        <div class="item-left">
+          <span>{{ numberItem }} items left</span>
+        </div>
+        <div class="option-filters">
+          <span class="tag" @click="filterTodoItem('all',$event)">All</span
+          ><span class="tag" @click="filterTodoItem('active',$event)"
+            >Active</span
+          ><span class="tag" @click="filterTodoItem('completed',$event)"
+            >Completed</span
+          >
+        </div>
+        <div class="clear-container" ><span @click="clearCompleted()">Clear Completed</span></div>
+      </div>
+    </div>
+    <p>Drag and drop to reorder list</p>
   </div>
 </template>
 
 <script>
-
+import TodoItem from "./components/TodoItem.vue";
 export default {
-  name: 'App',
-}
+  name: "App",
+  components: {
+    TodoItem,
+  },
+  data() {
+    return {
+      tabTodoItem: [
+        {
+          text: "Complete online Javascript-course",
+          completed: false,
+          active: false,
+          index: 0,
+          isSelected: false,
+          tabTag: ["all", "active"],
+          display: true,
+          textIsStrike:false
+        },
+      ],
+    };
+  },
+
+  methods: {
+    createDataItemTodo() {
+      let textItem = document.getElementsByClassName("input-text")[0].value;
+
+      let itemObejct = {
+        text: textItem,
+        completed: false,
+        active: false,
+        index: this.tabTodoItem.length,
+        isSelected: false,
+        tabTag: ["all", "active"],
+        display: true,
+        textIsStrike:false
+      };
+
+      this.tabTodoItem.push(itemObejct);
+    },
+
+    deleteObject(index) {
+      this.tabTodoItem.splice(index, 1);
+    },
+
+    getIndexObj(index) {
+      this.deleteObject(index);
+    },
+    updateData(index) {
+      let dataObjectTmp = this.tabTodoItem[index];
+      if (dataObjectTmp.isSelected) {
+        dataObjectTmp.isSelected = false;
+        dataObjectTmp.textIsStrike =false
+
+        dataObjectTmp.tabTag.splice(
+          dataObjectTmp.tabTag.indexOf("completed"),
+          1
+        );
+
+        dataObjectTmp.tabTag.push("active");
+      } else {
+        this.tabTodoItem[index].isSelected = true;
+        dataObjectTmp.textIsStrike =true
+        dataObjectTmp.tabTag.splice(dataObjectTmp.tabTag.indexOf("active"), 1);
+
+        dataObjectTmp.tabTag.push("completed");
+      }
+    },
+    filterTodoItem(tag,event) {
+      this.tabTodoItem.forEach((elt) => {
+        if (!elt.tabTag.includes(tag)) {
+          elt.display = false;
+        }else{
+          elt.display = true;
+        }
+      });
+      let tabSpan = Array.from(document.getElementsByClassName('tag'));
+
+      tabSpan.forEach(elt=>{
+        if(elt != event.target){
+          if(elt.classList.contains('blueTag')){
+            elt.classList.remove('blueTag')
+          }
+        }else{
+          if(!elt.classList.contains('blueTag'))
+          elt.classList.add('blueTag')
+        }
+      })
+    },
+    clearCompleted(){
+      for(let i = this.tabTodoItem.length-1;i>=0;i--){
+        if(this.tabTodoItem[i].tabTag.includes('completed')){
+
+          this.tabTodoItem.splice(i,1);
+        }
+      }
+    }
+  },
+  computed: {
+    numberItem() {
+      return this.tabTodoItem.length;
+    },
+  },
+};
 </script>
 
-<style lang="">
+<style lang="scss">
+#app {
+  min-height: 100vh;
+  min-width: 100vw;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: url("./assets/bg-desktop-light.jpg") no-repeat;
+  background-color: rgb(236, 234, 234);
+  background-blend-mode: multiply;
+  background-size: contain;
+  margin: 0;
+  padding: 0;
+  * {
+    font-family: "Commissioner", sans-serif;
+  }
+  .main-container {
+    width: 35vw;
+    height: fit-content;
+    min-height: 70vh;
+    border: 2px solid;
+    .header {
+      display: flex;
+      margin-bottom: 30px;
+      > * {
+        width: 50%;
+      }
+
+      .container-todo {
+        span {
+          letter-spacing: 0.6rem;
+          color: white;
+          text-transform: uppercase;
+          font-weight: bold;
+          font-size: 40px;
+        }
+      }
+      .containerDay-Night {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        img {
+          height: 25px;
+          width: 25px;
+        }
+        img:hover {
+          cursor: url("data:image/x-icon;base64,AAACAAEAICACAAAAAAAwAQAAFgAAACgAAAAgAAAAQAAAAAEAAQAAAAAAgAAAAAAAAAAAAAAAAgAAAAAAAAAAAAAA////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/gAAAf4AAAPVAAAH1QAAB9WAAA3VgAAd/4AAGbaAAAG2gAABtgAAAYAAAAGAAAABgAAAAYAAAAAAAAA//////////////////////////////////////////////////////////////////////////////////////gD///4A///8AP//+AB///AAf//wAD//4AA//8AAP//AAD//5AA///wAf//8Af///D////w////8P////n///8="),
+            auto;
+        }
+      }
+    }
+    .newTodo {
+      background-color: white;
+      min-height: 40px;
+      display: flex;
+      align-items: center;
+      padding-left: 20px;
+      border-radius: 5px;
+      margin: 20px 0;
+
+      .input-text {
+        border: none;
+        outline: none;
+        display: flex;
+        margin-left: 5px;
+      }
+      .input-radio {
+        height: 17px;
+        width: 17px;
+      }
+    }
+    .container-todolist {
+      background-color: white;
+      min-height: 50px;
+      border-radius: 5px;
+    }
+    .container-options {
+      display: flex;
+      align-items: center;
+      background-color: white;
+      min-height: 40px;
+      * {
+        color: gray;
+        font-size: 11px;
+        font-weight: bold;
+      }
+      .item-left {
+        width: 30%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+      .option-filters {
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+        width: 60%;
+
+        span:hover {
+          cursor: url("data:image/x-icon;base64,AAACAAEAICACAAAAAAAwAQAAFgAAACgAAAAgAAAAQAAAAAEAAQAAAAAAgAAAAAAAAAAAAAAAAgAAAAAAAAAAAAAA////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/gAAAf4AAAPVAAAH1QAAB9WAAA3VgAAd/4AAGbaAAAG2gAABtgAAAYAAAAGAAAABgAAAAYAAAAAAAAA//////////////////////////////////////////////////////////////////////////////////////gD///4A///8AP//+AB///AAf//wAD//4AA//8AAP//AAD//5AA///wAf//8Af///D////w////8P////n///8="),
+            auto;
+          color: rgb(56, 56, 218);
+        }
+        .blueTag{
+          color: rgb(56, 56, 218);
+        }
+      }
+      .clear-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 30%;
+        span:hover {
+          cursor: url("data:image/x-icon;base64,AAACAAEAICACAAAAAAAwAQAAFgAAACgAAAAgAAAAQAAAAAEAAQAAAAAAgAAAAAAAAAAAAAAAAgAAAAAAAAAAAAAA////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/gAAAf4AAAPVAAAH1QAAB9WAAA3VgAAd/4AAGbaAAAG2gAABtgAAAYAAAAGAAAABgAAAAYAAAAAAAAA//////////////////////////////////////////////////////////////////////////////////////gD///4A///8AP//+AB///AAf//wAD//4AA//8AAP//AAD//5AA///wAf//8Af///D////w////8P////n///8="),
+            auto;
+          color: rgb(56, 56, 218);
+        }
+      }
+    }
+  }
+  p {
+    text-align: center;
+    font-size: 12px;
+    color: gray;
+    font-weight: bold;
+  }
+}
+
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,139 @@
+<template>
+  <div v-if="dataObject.display" class="mainContent all">
+    <div class="container-input">
+      <input
+        type="radio"
+        :id="classNameItem"
+        name="drone"
+        value="huey"
+        class="input-radio"
+        @click="selectObject(dataObject.index, $event)"
+      />
+      <label
+        v-if="dataObject.isSelected"
+        :id="classNameItem"
+        :for="classNameItem"
+        ><i class="fas fa-check"></i
+      ></label>
+    </div>
+    <div class="container-text">
+      <p v-if="dataObject.textIsStrike" class="text-create textStrike">{{ dataObject.text }}</p>
+      <p v-else class="text-create">{{ dataObject.text }}</p>
+    </div>
+    <div class="close-container">
+      <img
+        @click="deleteObject(dataObject.index)"
+        src="../assets/icon-cross.svg"
+        alt=""
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: {
+    dataObject: Object,
+  },
+  computed: {
+    classNameItem() {
+      return "Item-" + this.dataObject.index;
+    },
+  },
+
+  methods: {
+    deleteObject(index) {
+      console.log(index);
+      this.$emit("setIndexObject", index);
+    },
+    selectObject(index, event) {
+      console.log(event.target);
+      event.target.checked = false;
+      console.log(index);
+      this.$emit("checkRadio", index);
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.mainContent {
+  display: flex;
+  border-bottom: 1px solid rgb(209, 209, 209);
+  .container-input {
+    width: 10%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+
+    input {
+      height: 17px;
+      width: 17px;
+    }
+    label {
+      height: 17px;
+      width: 17px;
+      background: linear-gradient(hsl(192, 100%, 67%), hsl(280, 87%, 65%));
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border-radius: 50%;
+      position: absolute;
+      left: 15px;
+      i {
+        padding: 0;
+      }
+    }
+    label:hover {
+      cursor: url("data:image/x-icon;base64,AAACAAEAICACAAAAAAAwAQAAFgAAACgAAAAgAAAAQAAAAAEAAQAAAAAAgAAAAAAAAAAAAAAAAgAAAAAAAAAAAAAA////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/gAAAf4AAAPVAAAH1QAAB9WAAA3VgAAd/4AAGbaAAAG2gAABtgAAAYAAAAGAAAABgAAAAYAAAAAAAAA//////////////////////////////////////////////////////////////////////////////////////gD///4A///8AP//+AB///AAf//wAD//4AA//8AAP//AAD//5AA///wAf//8Af///D////w////8P////n///8="),
+        auto;
+    }
+    input:hover {
+      cursor: url("data:image/x-icon;base64,AAACAAEAICACAAAAAAAwAQAAFgAAACgAAAAgAAAAQAAAAAEAAQAAAAAAgAAAAAAAAAAAAAAAAgAAAAAAAAAAAAAA////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/gAAAf4AAAPVAAAH1QAAB9WAAA3VgAAd/4AAGbaAAAG2gAABtgAAAYAAAAGAAAABgAAAAYAAAAAAAAA//////////////////////////////////////////////////////////////////////////////////////gD///4A///8AP//+AB///AAf//wAD//4AA//8AAP//AAD//5AA///wAf//8Af///D////w////8P////n///8="),
+        auto;
+      border-color: linear-gradient(hsl(192, 100%, 67%), hsl(280, 87%, 65%));
+      border-width: 4px;
+    }
+  }
+  .container-text {
+    width: 70%;
+    padding-left: 10px;
+
+    p {
+      display: inline-block;
+      text-align: left;
+      padding: 0;
+      justify-content: flex-end;
+    }
+  }
+  .close-container {
+    width: 10%;
+    // border: 2px solid;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    img {
+      display: none;
+      color: rgb(228, 225, 225);
+    }
+  }
+}
+.mainContent:hover {
+  cursor: url("data:image/x-icon;base64,AAACAAEAICACAAAAAAAwAQAAFgAAACgAAAAgAAAAQAAAAAEAAQAAAAAAgAAAAAAAAAAAAAAAAgAAAAAAAAAAAAAA////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/gAAAf4AAAPVAAAH1QAAB9WAAA3VgAAd/4AAGbaAAAG2gAABtgAAAYAAAAGAAAABgAAAAYAAAAAAAAA//////////////////////////////////////////////////////////////////////////////////////gD///4A///8AP//+AB///AAf//wAD//4AA//8AAP//AAD//5AA///wAf//8Af///D////w////8P////n///8="),
+    auto;
+  img {
+    display: flex;
+  }
+}
+
+.mainContent{
+  .textStrike{
+    text-decoration: line-through;
+    color: rgb(223, 217, 217) !important;
+  }
+}
+
+
+</style>
